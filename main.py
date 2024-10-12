0eebb94764ca176ef68d66476c077729cfd6c7d6
Elif Gök <elifgk148@gmail.com>
2024-10-12 05:13:47 +0300

    revision 2

--- main.py
+++ main.py
@@ -1,64 +1,91 @@
 import turtle
 import random
 import time
-from itertools import repeat
 from tkinter import messagebox, mainloop
 
 pencere= turtle.Screen()
-messagebox.showinfo("Kaplumbağayı Yakala","Oyun Başlıyoor")
+messagebox.showinfo("Kaplumbağayı Yakala","Oyun Başlıyor")
 pencere.bgcolor("light blue")
-pencere.title("Turtle Ödevi")
+pencere.title("Turtle")
 pencere.setup(700,700)
 
-kalem=turtle.Turtle()
-kalem.shape("turtle")
-kalem.shapesize(2,2,2)
-kalem.color("green","pink")
-kalem.penup()
-kalem.speed(10)
+hedef = turtle.Turtle()
+hedef.shape("turtle")
+hedef.shapesize(2,2,2)
+hedef.color("black","pink")
+hedef.penup()
+hedef.speed(10)
+sure=20
+skor= 0
 
+def tiklama(x,y):
+    global skor
+    skor = skor+1
+    skorum.clear()
+    style = ("Arial", 20, "bold")
+    skorum.write(skor,font=style)
 
-style= ("Arial",20,"bold")
-turtle.hideturtle()
-turtle.penup()
-turtle.goto(-110,300)
-turtle.write("Süre:",font=style)
-
-style= ("Arial",20,"bold")
-turtle.hideturtle()
-turtle.penup()
-turtle.goto(-110,250)
-turtle.write("Skor:",font=style)
-
-
-
+def kalan_zaman():
+    global sure
+    time.sleep(1)
+    sure=sure-1
+    kalan_sure.clear()
+    style = ("Arial", 20, "bold")
+    kalan_sure.write(sure,font=style)
 
 
 koordinat_x= [-50,-100,-150,-200,-250,-300,50,100,150,200,250,300]
 koordinat_y= [-50,-100,-150,-200,-250,-300,50,100,150,200,]
 
-sure=5
-skor= 0
+
+
+sure_yazisi=turtle.Turtle()
+style= ("Arial",20,"bold")
+sure_yazisi.hideturtle()
+sure_yazisi.penup()
+sure_yazisi.speed(10)
+sure_yazisi.goto(-110,300)
+sure_yazisi.write("Süre:",font=style)
+
+style= ("Arial",20,"bold")
+kalan_sure= turtle.Turtle()
+kalan_sure.hideturtle()
+kalan_sure.penup()
+kalan_sure.speed(10)
+kalan_sure.goto(100,300)
+kalan_sure.write(sure,font=style)
+
+
+
+
+skor_yazisi=turtle.Turtle()
+style= ("Arial",20,"bold")
+skor_yazisi.hideturtle()
+skor_yazisi.penup()
+skor_yazisi.speed(10)
+skor_yazisi.goto(-110,250)
+skor_yazisi.write("Skor:",font=style)
+
+style= ("Arial",20,"bold")
+skorum = turtle.Turtle()
+skorum.hideturtle()
+skorum.penup()
+skorum.speed(10)
+skorum.goto(100,250)
+skorum.write(skor,font=style)
 
 while sure >0:
-    kalem.goto(random.choice(koordinat_x),random.choice(koordinat_y))
-    time.sleep(1)
-    sure=sure-1
-    repeat
+    hedef.goto(random.choice(koordinat_x),random.choice(koordinat_y))
+    hedef.onclick(tiklama)
+    kalan_zaman()
+    hedef.home()
 
 if skor <3:
     messagebox.showinfo("Oyun Sonu","Kaybettiniz. Çıkmak için Q'ya basınız.")
 else:
-    messagebox.showinfo("Oyun Sonu","Tebrikler!! Kazandınız! Çıkmak için Q'ya basınız.")
+    messagebox.showinfo("Oyun Sonu","Tebrikler!! Kazandınız!! Çıkmak için Q'ya basınız.")
 
 pencere.listen()
 pencere.onkey(fun=exit,key= "q")
-#while sure > 0:
-
-
-
-
-### GOTO FONKSİYONU KULLANABİLİRSİN, TURTLEIN POZİSYONU  KOORDİNATLA BELİTLENİYOR
-#turtle.onclick(fonksiyon) tıklayınca bir artan fonksiyon yaz
 
 mainloop()
